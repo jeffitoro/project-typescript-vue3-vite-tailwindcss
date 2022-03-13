9a892d4053f33dea923bcfde5ee79c68e168b383
Jefferson <jeffersonorosco1@gmail.com>
2022-03-13 22:40:33 +0100

    start project: tailwindcss + typescript + structure directories added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
