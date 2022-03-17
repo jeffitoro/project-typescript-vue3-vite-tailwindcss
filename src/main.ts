ff4294463899272de0851e9a77faf9052ee9c417
Jefferson <jeffersonorosco1@gmail.com>
2022-03-17 14:54:13 +0100

    add package dompurify for vue 3 not finished implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueDOMPurifyHTML from 'vue-dompurify-html';
+
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueDOMPurifyHTML)
 
 app.mount('#app')
