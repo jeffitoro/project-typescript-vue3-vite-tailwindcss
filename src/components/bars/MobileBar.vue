1e797de2b816e6605309287aed6456e812abbddf
Jefferson <jeffersonorosco1@gmail.com>
2022-03-17 14:07:48 +0100

    add mobile bar with burger + side bar + transition + responsive

--- src/components/bars/MobileBar.vue
+++ src/components/bars/MobileBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex bg-gray-800 text-gray-100 justify-end md:hidden">
+  <div class="flex bg-blue-600 dark:bg-gray-800 text-gray-100 justify-end md:hidden">
     <button
       class="p-4 focus:outline-none focus:bg-gray-700"
       v-on:click="openSideBar"
--- src/components/bars/SideBar.vue
+++ src/components/bars/SideBar.vue
@@ -7,14 +7,23 @@
     leave-active-class="ease-out transition-medium"
     leave-to-class="opacity-0"
   >
-    <div @keydown.esc="openSideBar" v-show="open" class="z-10 fixed inset-0 transition-opacity">
-      <div @click="openSideBar" class="absolute inset-0 bg-black opacity-50" tabindex="0"></div>
+    <div
+      @keydown.esc="openSideBar"
+      v-show="open"
+      class="z-10 fixed inset-0 transition-opacity"
+    >
+      <div
+        @click="openSideBar"
+        class="absolute inset-0 bg-black opacity-50"
+        tabindex="0"
+      ></div>
     </div>
   </transition>
   <div
     class="
       z-20
       bg-blue-800
+      dark:bg-gray-700
       text-blue-100
       w-2/3
       md:w-1/3
@@ -30,14 +39,27 @@
       ease-in-out
       md:relative md:translate-x-0
     "
-    :class="props.open ? '' : '-translate-x-full'"
-  ></div>
+    :class="open ? '' : '-translate-x-full'"
+  >
+    <card-message
+      v-for="messageItem in messages"
+      :key="messageItem.id"
+      :message="messageItem"
+    >
+    </card-message>
+  </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
+import CardMessage from "@/components/cards/CardMessage.vue";
+import type { Message } from "@/types";
+import type { PropType } from "vue";
+
+defineProps({
   open: Boolean,
+  messages: Array as PropType<Message[]>,
 });
+
 const emit = defineEmits(["openSideBar"]);
 const openSideBar = () => {
   emit("openSideBar");
